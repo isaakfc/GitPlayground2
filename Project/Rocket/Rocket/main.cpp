d1b54cbffc3db6dc1ff76a72f8777bb0666d2a9b
Isaac Faraz-Chandler <izaacfc@yahoo.co.uk>
2024-02-01 21:09:30 +0000

    This is my first commit

---
+++ Project/Rocket/Rocket/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Rocket
+//
+//  Created by Isaac Faraz-Chandler on 01/02/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Hello, this is my rocket!!!!!\n";
+    return 0;
+}
