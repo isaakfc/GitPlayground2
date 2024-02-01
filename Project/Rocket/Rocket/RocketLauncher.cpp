c792f737eefe8134458139d5c7c215be5800afc6
Isaac Faraz-Chandler <izaacfc@yahoo.co.uk>
2024-02-01 21:39:03 +0000

    COMMIT 3

---
+++ Project/Rocket/Rocket/RocketLauncher.cpp
@@ -0,0 +1,11 @@
+//
+//  RocketLauncher.cpp
+//  Rocket
+//
+//  Created by Isaac Faraz-Chandler on 01/02/2024.
+//
+
+#include "RocketLauncher.hpp"
+
+std::cout << "BOOM!!!!!\n";
+std::cout << "BOOM!!!!!\n";
---
+++ Project/Rocket/Rocket/RocketLauncher.hpp
@@ -0,0 +1,13 @@
+//
+//  RocketLauncher.hpp
+//  Rocket
+//
+//  Created by Isaac Faraz-Chandler on 01/02/2024.
+//
+
+#ifndef RocketLauncher_hpp
+#define RocketLauncher_hpp
+
+#include <stdio.h>
+
+#endif /* RocketLauncher_hpp */
